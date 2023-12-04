0784a894a676a0dc2bae8ce97e0275bf5fd3887c
themms <maximusnewera@mail.ru>
2023-12-04 17:31:22 +0600

    deploy

---
+++ keyboards_buttons.py
@@ -0,0 +1,46 @@
+from aiogram import types
+
+buttonWeb = [
+    types.InlineKeyboardButton(text="Мобильная разработка", callback_data="web_mobile"),
+    types.InlineKeyboardButton(text="Сайты/Веб-Приложения", callback_data="web_web_app"),
+    types.InlineKeyboardButton(text="Десктопные приложения", callback_data="web_desktop_app"),
+    types.InlineKeyboardButton(text="Игры", callback_data="web_games")
+]
+
+buttonOOP = [
+    types.InlineKeyboardButton(text="Да", callback_data="OOP_yes"),
+    types.InlineKeyboardButton(text="Нет", callback_data="OOP_no"),
+]
+
+buttonPerformance = [
+    types.InlineKeyboardButton(text="1", callback_data="performance_1"),
+    types.InlineKeyboardButton(text="2", callback_data="performance_2"),
+    types.InlineKeyboardButton(text="3", callback_data="performance_3"),
+]
+
+buttonSecurity = [
+    types.InlineKeyboardButton(text="1", callback_data="security_1"),
+    types.InlineKeyboardButton(text="2", callback_data="security_2"),
+    types.InlineKeyboardButton(text="3", callback_data="security_3"),
+]
+
+buttonEntryLevel = [
+    types.InlineKeyboardButton(text="Низкий", callback_data="entry_level_low"),
+    types.InlineKeyboardButton(text="Высокий", callback_data="entry_level_high"),
+]
+
+buttonDevSpeed = [
+    types.InlineKeyboardButton(text="1", callback_data="development_speed_1"),
+    types.InlineKeyboardButton(text="2", callback_data="development_speed_2"),
+    types.InlineKeyboardButton(text="3", callback_data="development_speed_3"),
+]
+
+buttonCrossPlatform = [
+    types.InlineKeyboardButton(text="Да", callback_data="cross_platform_yes"),
+    types.InlineKeyboardButton(text="Нет", callback_data="cross_platform_no"),
+]
+
+buttonPart = [
+    types.InlineKeyboardButton(text="Серверная", callback_data="select_part_server"),
+    types.InlineKeyboardButton(text="Клиентская", callback_data="select_part_client"),
+]
---
+++ main.py
@@ -0,0 +1,73 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from config import API_TOKEN, start_text
+from aiogram.dispatcher.filters import Text
+from keyboards_buttons import *
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+replyes = []
+
+@dp.message_handler(commands=['start', 'help'])
+async def start(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Начать", callback_data="begin"))
+    await message.answer(start_text, reply_markup=keyboard)
+
+@dp.callback_query_handler(text="begin")
+async def begin(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttonWeb)
+    await call.message.edit_text("Укажи желаемое направление разработки", reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(startswith="web_"))
+async def OOP(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttonOOP)
+    await call.message.edit_text("Должна ли использоваться ООП парадигма в проекте?", reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(startswith="OOP_"))
+async def performance(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(*buttonPerformance)
+    await call.message.edit_text("Насколько важна производительность? ", reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(startswith="performance_"))
+async def security(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(*buttonSecurity)
+    await call.message.edit_text("Укажите необходимость высоких критериев безопасности", reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(startswith="security_"))
+async def entry_level(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttonEntryLevel)
+    await call.message.edit_text("Укажите порог входа", reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(startswith="entry_level_"))
+async def development_speed(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(*buttonDevSpeed)
+    await call.message.edit_text("Насколько важна скорость разработки?", reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(startswith="development_speed_"))
+async def cross_platform(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(*buttonCrossPlatform)
+    await call.message.edit_text("Важная ли кроссплатформенность? ", reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(startswith="cross_platform_"))
+async def select_part(call: types.CallbackQuery):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(*buttonPart)
+    await call.message.edit_text("Предпочитаемая часть разработки проектов", reply_markup=keyboard)
+
+@dp.callback_query_handler(Text(startswith="select_part_"))
+async def end(call: types.CallbackQuery):
+    await call.message.edit_text("Спасибо за ответы! \nТест окончен")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
